f28db0b8f447b4c3e2f9db45f07c8201ffa9ab5b
tosinwilliams <tosin_williams@outlook.com>
2018-07-03 17:05:52 +0100

    Added Forum, Message form, shop and orders page + database and shopping cart

---
+++ post_action.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+require('login_tools.php');
+
+if (!isset($_SESSION['user_id'])) {
+    load();
+}
+
+$page_title = 'Post Error';
+include ('includes/header.html');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($_POST['subject'])) {
+        echo'<p>Please enter a subject for this post.</p>';
+    }
+
+    if (empty($_POST['message'])) {
+        echo'<p>Please enter a message for this post.</p>';
+    }
+
+    if (!empty($_POST['subject']) && !empty($_POST['message'])) {
+        require ('../connect_db.php');
+        $q = "INSERT INTO forum
+            (first_name,last_name,subject,message,post_date)
+            VALUES(
+            '{$_SESSION[first_name]}',
+            '{$_SESSION[last_name]}',
+            '{$_POST[subject]}',
+            '{$_POST[message]}',
+            NOW())";
+        $r = mysqli_query($conn, $q);
+
+        if (mysqli_affected_rows($conn) != 1) {
+            echo '<p>Error</p>' . mysqli_error($conn);
+        } else {
+            load('forum.php');
+        }
+
+        echo '<p><a href="forum.php">Forum</a>';
+        include('includes/footer.html');
+    }
+}
+
